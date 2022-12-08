ed8c3648845b8604819c5a7f611e0e7344f5fdb4
Igoruxa007 <74563405+Igoruxa007@users.noreply.github.com>
2022-12-08 16:52:25 +0300

    Created users table with sqlalchemy

---
+++ db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+engine = create_engine("postgresql://okzvmcvb:Uez6Q6PUDNxgO1hwPeg_8mhaiOKyjcBu@mouse.db.elephantsql.com:5432/okzvmcvb")
+db_session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
